f9332d2efd7499b555eea7d859362fd174af9e92
WendonScott <fedimaybe@protonmail.com>
2022-04-20 23:08:20 +0200

    Refactor: Add carousel to replace images gallery

--- js/js.js
+++ js/js.js
@@ -26,24 +26,20 @@ const navSlide = () =>{
 navSlide();
 
 // javaScript for gallery
-const lightbox = document.createElement('div')
-lightbox.id = 'lightbox'
-document.body.appendChild(lightbox)
+const buttons = document.querySelectorAll("[data-carousel-button]")
 
-const images = document.querySelectorAll('img')
-images.forEach(image =>{
-  image.addEventListener('click', e =>{
-    lightbox.classList.add('active')
-    const img = document.createElement('img')
-    img.src = image.src
-    while (lightbox.firstChild) {
-      lightbox.removeChild(lightbox.firstChild)
-    }
-    lightbox.appendChild(img)
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1
+    const slides = button.closest("[data-carousel]").querySelector("[data-slides]")
+
+
+    const activeSlide = slides.querySelector("[data-active]")
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset
+    if (newIndex < 0) newIndex = slides.children.length - 1 
+    if (newIndex >= slides.children.length) newIndex = 0
+
+    slides.children[newIndex].dataset.active = true
+    delete activeSlide.dataset.active
   })
-})
-
-lightbox.addEventListener('click', e =>{
-  if (e.target !== e.currentTarget) return
-  lightbox.classList.remove('active')
 })
